98d6dadb671b2bae4395b42fbfd17bbebbb04f30
Aryaarun <aryaarunnair@gmail.com>
2024-05-28 18:40:14 +0300

    first commit

---
+++ python.py
@@ -0,0 +1,10 @@
+import random
+def generate_otp(length=6):
+    otp = ''
+    for _ in range(length):
+        otp += str(random.randint(0, 9))
+    return otp
+# Test the function
+if __name__ == "__main__":
+    otp = generate_otp()
+    print("Generated OTP:", otp)
